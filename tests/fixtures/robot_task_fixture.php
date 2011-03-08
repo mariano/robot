c098b5c04ddf460ff32989b59358db6bc9139ece
Rob Wilkerson <rob@robwilkerson.org>
2011-03-08 07:07:31 -0500

    BugzID 6340: Adding empty fixtures to support unit test dependencies.

---
+++ tests/fixtures/robot_task_fixture.php
@@ -0,0 +1,7 @@
+<?php
+class RobotTaskFixture extends CakeTestFixture {
+    public $name = 'RobotTask';
+    public $import = array('table' => 'robot_tasks');
+    public $records = array(/** none, initially */);
+}
+?>
